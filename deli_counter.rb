d9aef607e5dd930a0f8e84d5d007129ebe5b5597
Jobsidney <jobsidney67@gmail.com>
2022-08-18 02:09:07 +0300

    commit tree 8ef38c71d6f2eef51c829968c4e0b92992b87db7

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,41 @@
 # Write your code here.
+katz_deli = [{name:"job"},{name:"Shee"}]
+def line(people)
+    lineP=[]
+    if people.length==0
+        "The line is currently empty."
+    elsif people.length>0
+        people.each{
+            |person|
+            lineP.push(person[:name])
+        }
+        newP=lineP.map.with_index{
+            |name,index|
+            "#{index+1}. #{name}"
+        }
+        
+        "The line is currently: #{newP.join(" ")}"
+    end
+end
+p line(katz_deli)
+
+
+def take_a_number(current,name)
+    puts "Welcome, #{name}. You are number #{current.length+1} in line."
+    katz_deli.push({name:name})
+end
+take_a_number(katz_deli,"chris")
+
+def now_serving(serve)
+    unless serve.length=0
+        puts "There is nobody waiting to be served!"
+    else
+        serve.map{
+            |item|
+            puts "Currently serving #{item[:name]}."
+            serve.shift()
+        }
+    end
+end
+
+now_serving(katz_deli)
